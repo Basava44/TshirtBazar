bd38b031a361c763e3dcb28ce8afe96128e7fd72
Karibasaveshwara_TG <karibasaveshwara.tg@moonraft.com>
2024-02-03 00:10:48 +0530

    route update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {
     path: 'dashboard',
     loadChildren: () =>
@@ -15,6 +14,8 @@ const routes: Routes = [
     loadChildren: () =>
       import('./components/bag/bag.module').then((m) => m.BagModule),
   },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dashboard' },
 ];
 
 @NgModule({
